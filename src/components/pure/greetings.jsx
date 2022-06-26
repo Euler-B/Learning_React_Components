0aec153fd17bbd8d9d533212d7b00ef8349f23da
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-06-25 21:47:29 -0400

    First component

---
+++ src/components/pure/greetings.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greetings extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+Greetings.propTypes = {
+
+};
+
+
+export default Greetings;
